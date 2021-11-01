ff9282d39a1f532aef474fa2a04b476deb1eac0a
darianfd99 <darianfd99@gmail.com>
2021-11-01 16:03:46 +0500

    Blockchain created

--- block.go
+++ block.go
@@ -3,7 +3,6 @@ package main
 import (
 	"bytes"
 	"crypto/sha256"
-	"fmt"
 	"strconv"
 	"time"
 )
@@ -11,13 +10,13 @@ import (
 type Block struct {
 	Timestamp     int64
 	Data          []byte
-	PrevBLockHash []byte
+	PrevBlockHash []byte
 	Hash          []byte
 }
 
 func (b *Block) SetHash() {
 	timestamp := []byte(strconv.FormatInt(b.Timestamp, 10))
-	headers := bytes.Join([][]byte{b.PrevBLockHash, b.Data, timestamp}, []byte{})
+	headers := bytes.Join([][]byte{b.PrevBlockHash, b.Data, timestamp}, []byte{})
 	hash := sha256.Sum256(headers)
 	b.Hash = hash[:]
 }
@@ -26,18 +25,10 @@ func NewBlock(data string, prevBlockHash []byte) *Block {
 	block := &Block{
 		Timestamp:     time.Now().Unix(),
 		Data:          []byte(data),
-		PrevBLockHash: []byte(prevBlockHash),
+		PrevBlockHash: []byte(prevBlockHash),
 		Hash:          []byte{},
 	}
 
 	block.SetHash()
 	return block
-}
-
-func main() {
-	b := NewBlock("Probando nuestro bloque", []byte("65dfs4"))
-	fmt.Println("Timestamp", b.Timestamp)
-	fmt.Println("Data:", string(b.Data))
-	fmt.Println("PrevBLockHash:", string(b.PrevBLockHash))
-	fmt.Println("Hash:", string(b.Hash))
 }
---
+++ main.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+func main() {
+	bc := NewBlockchain()
+	bc.AddBlock("Probando nuestro bloque")
+	bc.AddBlock("Enviamos 5 BTC a Marcos")
+	bc.AddBlock("Vamos")
+
+	for _, block := range bc.blocks {
+		fmt.Printf("Hash B.A: %x\n", block.PrevBlockHash)
+		fmt.Printf("Data: %s\n", block.Data)
+		fmt.Printf("Hash : %x\n\n", block.Hash)
+
+	}
+
+}
